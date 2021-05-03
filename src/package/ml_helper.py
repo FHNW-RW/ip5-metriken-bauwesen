cee8c32fe9e3964f83bcba96605fcbdd29bbd76a
Ralf Winkelmann <ralf.winkelmann@hotmail.com>
2021-05-03 20:33:35 +0200

    - created notebooks for linear regression and random forest models

--- src/package/ml_helper.py
+++ src/package/ml_helper.py
@@ -1,4 +1,23 @@
+from pandas import DataFrame
 from sklearn.metrics import mean_absolute_error, mean_squared_error, max_error
+from sklearn.preprocessing import LabelEncoder as Le
+
+import src.package.importer as im
+
+
+def hnf_dataset(df: DataFrame):
+    """ Returns dataset to estimate HNF based on GF and usage cluster """
+    relevant_features = df.copy().loc[:, [im.FIELD_AREA_TOTAL_FLOOR_416, im.FIELD_AREA_MAIN_USAGE, im.FIELD_USAGE_CLUSTER]]
+
+    # preprocess dataset
+    relevant_features = relevant_features.dropna(how="any")
+    relevant_features[im.FIELD_USAGE_CLUSTER] = Le().fit_transform(relevant_features[im.FIELD_USAGE_CLUSTER])
+
+    # features / labels
+    X = relevant_features[[im.FIELD_AREA_TOTAL_FLOOR_416, im.FIELD_USAGE_CLUSTER]]
+    y = relevant_features[im.FIELD_AREA_MAIN_USAGE]
+
+    return X, y
 
 
 def l_reg_evaluation(predictions, actual, r2_value, preview_count=10):
