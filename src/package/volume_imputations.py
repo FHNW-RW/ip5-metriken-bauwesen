b86893a161fb44b3dc308b35dd29a285d478e74a
Dietiker Luca (s) <luca.dietiker@students.fhnw.ch>
2021-06-08 15:14:18 +0200

    Add functions for numerical imputation.

--- src/package/volume_imputations.py
+++ src/package/volume_imputations.py
@@ -1,31 +1,53 @@
 from pandas import DataFrame
 
 import src.package.consts as c
-import src.package.importer_usages as im_usages
 
 
-def impute_mean(df: DataFrame, clustered: bool = True, percentile: float = 0.5):
-    """ replaces missing values of volume_total_416 with mean of respecting volume_total_116 if present """
-    df = df[[c.FIELD_USAGE_CLUSTER, c.FIELD_VOLUME_TOTAL_416, c.FIELD_VOLUME_TOTAL_116]]
+def impute_mean(df: DataFrame, field: str = "", other: str = "", clustered: bool = True, percentile: float = 0.5):
+    """ fills missing values for specified field based on (clustered) mean values of other column """
 
-    # calc imputation ratios
+    if field is "":
+        field = c.FIELD_VOLUME_TOTAL_116
+        other = c.FIELD_VOLUME_TOTAL_416
+
+    if field is c.FIELD_VOLUME_TOTAL_416:
+        other = c.FIELD_VOLUME_TOTAL_116
+
+    # compute desired multiplication factors
+    imp_df = __get_imputation_factors(df,  field, other, clustered, percentile)
+
+    # if clustered, fill based on clustering
+    if clustered:
+        df_cpy = df.groupby(c.FIELD_USAGE_CLUSTER).apply(
+            lambda x: __apply_cluster_mean(x, x[c.FIELD_USAGE_CLUSTER].iloc[0], field, other, imp_df))
+        return df_cpy
+
+    # fill unclustered
+    return df.apply(
+        lambda x: __apply_mean(x, field, other, imp_df))
+
+
+def __get_imputation_factors(df, field, other, clustered, percentile):
     imp_df = df.copy()
-    imp_df['volume_total_116_416_ratio'] = (imp_df[c.FIELD_VOLUME_TOTAL_116] - imp_df[c.FIELD_VOLUME_TOTAL_416]) / imp_df[
-        c.FIELD_VOLUME_TOTAL_116]
-    imp_df = imp_df[[c.FIELD_USAGE_CLUSTER, c.FIELD_VOLUME_TOTAL_116]]
-    imp_df = imp_df.groupby(c.FIELD_USAGE_CLUSTER).describe()
-    imp_df = imp_df[c.FIELD_VOLUME_TOTAL_116+"/"+"{:.0%}".format(percentile)]
+    imp_df['filler_ratio'] = (imp_df[field] / imp_df[other])
 
     if clustered:
-        df = df.groupby(c.FIELD_USAGE_CLUSTER).apply(lambda x: __apply_cluster_mean(x, x[c.FIELD_USAGE_CLUSTER].iloc[0], imp_df, percentile))
+        imp_df = imp_df[[c.FIELD_USAGE_CLUSTER, 'filler_ratio']]
+        imp_df = imp_df.groupby(c.FIELD_USAGE_CLUSTER).describe().reset_index()
+        imp_df.columns = imp_df.columns.droplevel(0)
+        return imp_df[["", "{:.0%}".format(percentile)]]
 
-    # TODO else
-
-    return df
+    return imp_df['filler_ratio'].describe()["{:.0%}".format(percentile)]
 
 
-def __apply_cluster_mean(grp, grp_name, imps):
-    multi = imps.loc[(imps.index == grp_name)]
-    grp[c.FIELD_VOLUME_TOTAL_416].fillna(grp[c.FIELD_VOLUME_TOTAL_116] * multi)
+def __apply_cluster_mean(grp, grp_name, field, other, imps):
+    factor = imps[imps[""] == grp_name].iloc[:, 1]
+    grp[field] = grp[field].fillna(grp[other] * float(factor))
 
     return grp
+
+
+def __apply_mean(df, field, other, factor):
+    df[field] = df[field].fillna(df[other] * float(factor))
+
+    return df
